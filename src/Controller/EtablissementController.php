c2421d9e4c773954127dd9f7daf53601ce6f5c86
Ahmed Tamani <ahmed.tamani@etu.univ-lehavre.fr>
2025-03-14 15:09:34 +0100

    L'ajout des routes de visualisations

--- src/Controller/EtablissementController.php
+++ src/Controller/EtablissementController.php
@@ -15,4 +15,47 @@ final class EtablissementController extends AbstractController
             'controller_name' => 'EtablissementController',
         ]);
     }
+    #[Route('/etablissements/departement/{departement}', name: 'app_etablissements_departement')]
+    public function listByDepartement(string $departement, EtablissementRepository $repository): Response
+    {
+        $etablissements = $repository->findBy(['departement' => $departement]);
+
+        return $this->render('etablissement/list_by_departement.html.twig', [
+            'etablissements'  => $etablissements,
+            'departement'     => $departement,
+        ]);
+    }
+
+    #[Route('/etablissements/region/{region}', name: 'app_etablissements_region')]
+    public function listByRegion(string $region, EtablissementRepository $repository): Response
+    {
+        $etablissements = $repository->findBy(['region' => $region]);
+
+        return $this->render('etablissement/list_by_region.html.twig', [
+            'etablissements' => $etablissements,
+            'region'         => $region,
+        ]);
+    }
+
+    #[Route('/etablissements/commune/{commune}', name: 'app_etablissements_commune')]
+    public function listByCommune(string $commune, EtablissementRepository $repository): Response
+    {
+        $etablissements = $repository->findBy(['commune' => $commune]);
+
+        return $this->render('etablissement/list_by_commune.html.twig', [
+            'etablissements' => $etablissements,
+            'commune'        => $commune,
+        ]);
+    }
+
+    #[Route('/etablissements/academie/{academie}', name: 'app_etablissements_academie')]
+    public function listByAcademie(string $academie, EtablissementRepository $repository): Response
+    {
+        $etablissements = $repository->findBy(['academie' => $academie]);
+
+        return $this->render('etablissement/list_by_academie.html.twig', [
+            'etablissements' => $etablissements,
+            'academie'       => $academie,
+        ]);
+    }
 }
