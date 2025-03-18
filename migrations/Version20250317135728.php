931a11c1b71098e15f32a6ab62dc209c1f1dc74d
Mehdi <mehdilaz109@gmail.com>
2025-03-18 15:43:33 +0100

    Modification de la méthode load de la classe AppFixtures pour la lecture du fichier donnees.csv

---
+++ migrations/Version20250317135728.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250317135728 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE etablissement (id INT AUTO_INCREMENT NOT NULL, appellation_officiel VARCHAR(255) NOT NULL, denomination_principale VARCHAR(255) NOT NULL, longitude DOUBLE PRECISION NOT NULL, adresse VARCHAR(255) DEFAULT NULL, departement VARCHAR(255) NOT NULL, commune VARCHAR(150) NOT NULL, region VARCHAR(200) NOT NULL, academie VARCHAR(200) NOT NULL, date_ouverture DATE NOT NULL, secteur LONGTEXT NOT NULL COMMENT \'(DC2Type:simple_array)\', code_departement INT NOT NULL, code_region INT NOT NULL, code_academie INT NOT NULL, code_commune INT NOT NULL, latitude DOUBLE PRECISION NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE etablissement');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
