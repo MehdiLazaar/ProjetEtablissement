b7f313dc45c8148912643ae17056dd427fb67460
Mehdi <mehdilaz109@gmail.com>
2025-03-18 17:28:10 +0100

    Execution de la migration

--- migrations/Version20250318162739.php
+++ migrations/Version20250318162739.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250318141456 extends AbstractMigration
+final class Version20250318162739 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,13 +20,17 @@ final class Version20250318141456 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE commentaire (id INT AUTO_INCREMENT NOT NULL, etablissement_id INT DEFAULT NULL, auteur VARCHAR(255) DEFAULT NULL, date_creation DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', note INT DEFAULT NULL, texte_commentaire VARCHAR(255) DEFAULT NULL, INDEX IDX_67F068BCFF631228 (etablissement_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE etablissement (id INT AUTO_INCREMENT NOT NULL, appellation_officiel VARCHAR(255) NOT NULL, denomination_principale VARCHAR(255) NOT NULL, longitude DOUBLE PRECISION NOT NULL, adresse VARCHAR(255) DEFAULT NULL, departement VARCHAR(255) NOT NULL, commune VARCHAR(150) NOT NULL, region VARCHAR(200) NOT NULL, academie VARCHAR(200) NOT NULL, date_ouverture DATE NOT NULL, secteur LONGTEXT NOT NULL COMMENT \'(DC2Type:simple_array)\', code_departement INT NOT NULL, code_region INT NOT NULL, code_academie INT NOT NULL, code_commune INT NOT NULL, latitude DOUBLE PRECISION NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE commentaire ADD CONSTRAINT FK_67F068BCFF631228 FOREIGN KEY (etablissement_id) REFERENCES etablissement (id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE commentaire DROP FOREIGN KEY FK_67F068BCFF631228');
+        $this->addSql('DROP TABLE commentaire');
         $this->addSql('DROP TABLE etablissement');
         $this->addSql('DROP TABLE messenger_messages');
     }
