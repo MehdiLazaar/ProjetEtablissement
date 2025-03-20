44222b4bc1c955639be7dc2529a02817dbc01aeb
Mehdi <mehdilaz109@gmail.com>
2025-03-20 14:40:26 +0100

    Changement de qlq noms de route pour ne pas avoir un conflit

--- src/Controller/EtablissementController.php
+++ src/Controller/EtablissementController.php
@@ -10,13 +10,14 @@ use Symfony\Component\Routing\Attribute\Route;
 final class EtablissementController extends AbstractController
 {
     #[Route('/etablissement', name: 'app_etablissement')]
-    public function index(): Response
+    public function index(EtablissementRepository $repo): Response
     {
+        $etablissements = $repo->findAll();
         return $this->render('etablissement/index.html.twig', [
-            'controller_name' => 'EtablissementController',
+            'etablissements' => $etablissements,
         ]);
     }
-    #[Route('/etablissements/departement/{departement}', name: 'app_etablissements_departement')]
+    #[Route('/etablissements/departement/{departement}', name: 'app_etablissements_departement_nom')]
     public function listeParDepartement(string $departement, EtablissementRepository $repository): Response
     {
         $etablissements = $repository->findBy(['departement' => $departement]);
@@ -27,7 +28,7 @@ final class EtablissementController extends AbstractController
         ]);
     }
 
-    #[Route('/etablissements/region/{region}', name: 'app_etablissements_region')]
+    #[Route('/etablissements/region/{region}', name: 'app_etablissements_region_nom')]
     public function listeParRegion(string $region, EtablissementRepository $repository): Response
     {
         $etablissements = $repository->findBy(['region' => $region]);
@@ -38,7 +39,7 @@ final class EtablissementController extends AbstractController
         ]);
     }
 
-    #[Route('/etablissements/commune/{commune}', name: 'app_etablissements_commune')]
+    #[Route('/etablissements/commune/{commune}', name: 'app_etablissements_commune_nom')]
     public function listeParCommune(string $commune, EtablissementRepository $repository): Response
     {
         $etablissements = $repository->findBy(['commune' => $commune]);
@@ -49,7 +50,7 @@ final class EtablissementController extends AbstractController
         ]);
     }
 
-    #[Route('/etablissements/academie/{academie}', name: 'app_etablissements_academie')]
+    #[Route('/etablissements/academie/{academie}', name: 'app_etablissements_academie_nom')]
     public function listeParAcademie(string $academie, EtablissementRepository $repository): Response
     {
         $etablissements = $repository->findBy(['academie' => $academie]);
@@ -59,7 +60,7 @@ final class EtablissementController extends AbstractController
             'academie'       => $academie,
         ]);
     }
-    #[Route('/etablissements/departement/{code_departement}', name: 'app_etablissements_departement')]
+    #[Route('/etablissements/departement/code/{code_departement}', name: 'app_etablissements_departement_code')]
     public function listeParCodeDepartement(int $code_departement, EtablissementRepository $repository): Response
     {
         $etablissements = $repository->findBy(['code_departement' => $code_departement]);
@@ -70,7 +71,7 @@ final class EtablissementController extends AbstractController
         ]);
     }
 
-    #[Route('/etablissements/commune/{code_commune}', name: 'app_etablissements_commune')]
+    #[Route('/etablissements/commune/code/{code_commune}', name: 'app_etablissements_commune_code')]
     public function listeParCodeCommune(int $code_commune, EtablissementRepository $repository): Response
     {
         $etablissements = $repository->findBy(['code_commune' => $code_commune]);
@@ -81,7 +82,7 @@ final class EtablissementController extends AbstractController
         ]);
     }
 
-    #[Route('/etablissements/region/{code_region}', name: 'app_etablissements_region')]
+    #[Route('/etablissements/region/code/{code_region}', name: 'app_etablissements_region_code')]
     public function listeParCodeRegion(int $code_region, EtablissementRepository $repository): Response
     {
         $etablissements = $repository->findBy(['code_region' => $code_region]);
@@ -92,7 +93,7 @@ final class EtablissementController extends AbstractController
         ]);
     }
 
-    #[Route('/etablissements/academie/{code_academie}', name: 'app_etablissements_academie')]
+    #[Route('/etablissements/academie/code/{code_academie}', name: 'app_etablissements_academie_code')]
     public function listeParCodeAcademie(int $code_academie, EtablissementRepository $repository): Response
     {
         $etablissements = $repository->findBy(['code_academie' => $code_academie]);
